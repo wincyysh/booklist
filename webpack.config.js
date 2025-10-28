8b8494b2e9d19c0498ea2f70784507f83cc32eba
wincyysh <141292574+wincyysh@users.noreply.github.com>
2025-10-28 23:23:59 +0100

    from development to production build for GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   entry: './index.js',
-  mode: 'development',
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index_bundle.js',
